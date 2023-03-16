bffa17652216a492a95216ac800c9447281ae5a1
Hamza AitBamoh <hamzaaitbamoh@gmail.com>
2023-03-16 12:57:36 +0100

    fixed N+1 problem for author

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,8 +3,7 @@
     {{-- @dd($loop) --}}
         <article>
             <h1><a href="/posts/{{$post->slug}}"> {{$post->title}}</a></h1>
-            <p> By <a href="/users/{{$post->user->slug}}"> {{$post->user->name}}</a> </p>
-            <p> <a href="/categories/{{$post->category->slug}}"> {{$post->category->name}} </a></p>
+            <p> By <a href="/users/{{$post->author->slug}}"> {{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}"> {{$post->category->name}} </a></p>
             <div>
                 {!!$post->excerpt!!}
             </div>
